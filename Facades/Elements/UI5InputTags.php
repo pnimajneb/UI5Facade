0a9cf50052aed3d867a722a725562b285576e698
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-05-05 09:09:25 +0200

    NEW widget InputTags

---
+++ Facades/Elements/UI5InputTags.php
@@ -0,0 +1,14 @@
+<?php
+namespace exface\UI5Facade\Facades\Elements;
+
+/**
+ * Widget InputTags is the same as InputComboTable in UI5 
+ *
+ * @method InputTags getWidget()
+ * 
+ * @author Andrej Kabachnik
+ *        
+ */
+class UI5InputTags extends UI5InputComboTable
+{
+}
