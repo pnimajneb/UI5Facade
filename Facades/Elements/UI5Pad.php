4ee060c823236ec01aa121d976af5674a19bb041
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-07-27 14:40:15 +0200

    NEW widget Pad

---
+++ Facades/Elements/UI5Pad.php
@@ -0,0 +1,88 @@
+<?php
+namespace exface\UI5Facade\Facades\Elements;
+
+/**
+ * Generates a sap.m.Panel intended to contain tiles (see. UI5Tile).
+ * 
+ * @method \exface\Core\Widgets\Pad getWiget()
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class UI5Pad extends UI5Container
+{
+    
+    public function buildJsConstructor($oControllerJs = 'oController') : string
+    {
+        $childConstructorsJs = $this->buildJsChildrenConstructors();
+        
+        $panel = <<<JS
+        
+                new sap.m.Panel("{$this->getId()}", {
+                    content: [
+                        {$childConstructorsJs}
+                    ],
+                    {$this->buildJsProperties()}
+                }).addStyleClass("{$this->buildCssElementClass()}")
+                
+JS;
+        if ($this->getWidget()->getCenterContent() === true) {
+            $panel = $this->buildJsCenterWrapper($panel);
+        }
+                
+        if ($this->hasPageWrapper() === true) {
+            return $this->buildJsPageWrapper($panel);
+        }
+        
+        return $panel;
+    }
+    
+    protected function buildJsCenterWrapper(string $content) : string
+    {
+        return <<<JS
+        
+                        new sap.m.FlexBox({
+                            {$this->buildJsPropertyHeight()}
+                            {$this->buildJsPropertyVisibile()}
+                            justifyContent: "Center",
+                            alignItems: "Center",
+                            items: [
+                                {$content}
+                            ]
+                        })
+                        
+JS;
+    }
+    
+    protected function buildJsPropertyHeaderText() : string
+    {
+        if ($caption = $this->getCaption()) {
+            return <<<JS
+            
+                    headerText: "{$caption}",
+                    
+JS;
+        }
+        return '';
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Facades\AbstractAjaxFacade\Elements\AbstractJqueryElement::buildCssElementClass()
+     */
+    public function buildCssElementClass()
+    {
+        return 'exf-pad';
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5AbstractElement::getNeedsContainerContentPadding()
+     */
+    public function getNeedsContainerContentPadding() : bool
+    {
+        return false;
+    }
+}
