a96d2c8c96ed0f93ee46c61aba15000271cf88c9
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2022-03-31 14:17:17 +0200

    FIX conditional properties for ButtonGroup did not work

--- Facades/Elements/UI5ButtonGroup.php
+++ Facades/Elements/UI5ButtonGroup.php
@@ -4,14 +4,32 @@ namespace exface\UI5Facade\Facades\Elements;
 use exface\Core\Facades\AbstractAjaxFacade\Elements\JqueryButtonGroupTrait;
 
 /**
- * The AdminLTE implementation of the ButtonGroup widget
+ * UI5 does not have anything comparable to a button group, so the actual rendering takes place in UI5Toolbar
  *
  * @author Andrej Kabachnik
  *        
- * @method Toolbar getWidget()
+ * @method \exface\Core\Widgets\ButtonGroup getWidget()
  */
 class UI5ButtonGroup extends UI5AbstractElement
 {
     use JqueryButtonGroupTrait;
+    
+    protected function init()
+    {
+        parent::init();
+        // Since there is no UI5 control for the button group, we need to pass conditional properties
+        // to each individual button
+        // FIXME this will overwrite the conditional property of the button if it has one, so
+        // it would be better to add the some how. But how?
+        if ($condProp = $this->getWidget()->getHiddenIf()) {
+            foreach ($this->getWidget()->getButtons() as $btn) {
+                $btn->setHiddenIf($condProp->exportUxonObject());
+            }
+        }
+        if ($condProp = $this->getWidget()->getDisabledIf()) {
+            foreach ($this->getWidget()->getButtons() as $btn) {
+                $btn->setDisabledIf($condProp->exportUxonObject());
+            }
+        }
+    }
 }
-?>
