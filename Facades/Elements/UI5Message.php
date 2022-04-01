932ff54fc1271c5f60b79bfb916c60ca8ede6a7c
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2022-04-01 13:40:46 +0200

    FIX line breaks in Message widget

--- Facades/Elements/UI5Message.php
+++ Facades/Elements/UI5Message.php
@@ -44,11 +44,11 @@ JS;
     public function buildJsValue()
     {
         if (! $this->isValueBoundToModel()) {
-            $value = str_replace("\n", '', $this->getWidget()->getValue());
+            $value = $this->getWidget()->getValue();
             if ($caption = $this->getCaption()) {
                 $value = '<strong>' . $caption . ': </strong> ' . $value;
             }
-            $value = '"' . $this->escapeJsTextValue($value) . '"';
+            $value = '"' . nl2br($this->escapeJsTextValue($value)) . '"';
         } else {
             $value = $this->buildJsValueBinding();
         }
