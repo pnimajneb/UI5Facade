2e70ee1edc4b1f12286c253947ce2f4fbff5fe1d
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-01-03 11:51:41 +0100

    DEV PWA model now based on actions

--- Facades/UI5PWA.php
+++ Facades/UI5PWA.php
@@ -15,6 +15,17 @@ use exface\Core\Interfaces\Facades\FacadeInterface;
 use exface\Core\Interfaces\Actions\iReadData;
 use exface\Core\DataTypes\OfflineStrategyDataType;
 use exface\Core\Actions\ShowHelpDialog;
+use exface\Core\Actions\ShowWidget;
+use exface\Core\Interfaces\Actions\ActionInterface;
+use exface\Core\Interfaces\Widgets\iSupportLazyLoading;
+use exface\Core\Actions\ReadPrefill;
+use exface\Core\Widgets\Dialog;
+use exface\Core\Widgets\Data;
+use exface\Core\Actions\RefreshWidget;
+use exface\Core\Actions\CallWidgetFunction;
+use exface\Core\Interfaces\Widgets\iUseInputWidget;
+use exface\Core\Interfaces\Actions\iExportData;
+use exface\Core\Interfaces\Actions\iNavigate;
 
 /**
  * 
@@ -38,44 +49,106 @@ class UI5PWA extends AbstractPWA
      */
     protected function generateModelForWidget(WidgetInterface $widget, int $linkDepth = 100) : \Generator
     {
+        if ($widget->getId() === 'lagerTable' || $widget->getId() === 'scanWizard') {
+            $b =1;
+        }
         if ($widget->hasParent() === false) {
+            if ($widget->getPage()->isPublished() === false && $this->isAvailableOfflineUnpublished() === false) {
+                return;
+            }
             $this->addRoute(new PWARoute($this, $this->getViewForWidget($widget)->getRouteName(), $widget));
             yield 'Root widget for page ' . $widget->getPage()->getAliasWithNamespace() . ': ' . $widget->getId();
         }
         
-        if ($linkDepth > 0) {
-            foreach ($widget->getChildren() as $child) {
-                if ($child instanceof iTriggerAction && $child->hasAction() && $child->getAction() instanceof iShowWidget) {
-                    if (null !== $childActionWidget = $child->getAction()->getWidget()) {
-                        $this->addRoute(new PWARoute($this, $this->getViewForWidget($childActionWidget)->getRouteName(), $childActionWidget));
-                        yield 'Action widget for page ' . $widget->getPage()->getAliasWithNamespace() . ': ' . $widget->getId();
-                        yield from $this->generateModelForWidget($childActionWidget, ($linkDepth-1));
-                    } elseif (null !== $childActionPage = $child->getAction()->getPage()) {
-                        yield from $this->generateModelForWidget($childActionPage->getWidgetRoot(), ($linkDepth-1));
+        if ($linkDepth <= 0) {
+            return;
+        }
+        
+        foreach ($widget->getChildren() as $child) {
+            switch (true) {
+                case $child instanceof iSupportLazyLoading:
+                    if ($this->isWidgetLazyLoading($child) && $child->hasAction()) {
+                        $action = $child->getAction();
+                        $child->getLazyLoadingAction();
+                        $this->addAction($action, $child);
+                        if ($action->getOfflineStrategy() === null || $action->getOfflineStrategy() === OfflineStrategyDataType::PRESYNC) {
+                            $data = $action instanceof ReadPrefill ? $child->prepareDataSheetToPrefill() : $child->prepareDataSheetToRead();
+                            $this->addData($data, $action, $child);
+                        }
                     }
-                } else {
                     yield from $this->generateModelForWidget($child, ($linkDepth-1));
-                }
+                    break;
+                case $child instanceof iTriggerAction:
+                    if (! $child->hasAction()) {
+                        break;
+                    }
+                    $action = $child->getAction();
+                    $this->addAction($action, $child);
+                    switch (true) {
+                        case $action instanceof iReadData:
+                        case $child instanceof iSupportLazyLoading && $child->getLazyLoadingAction() === $action:
+                            $inputWidget = $child instanceof iUseInputWidget ? $child->getInputWidget() : $child;
+                            if ($this->getActionOfflineStrategy($action) === OfflineStrategyDataType::PRESYNC) {
+                                $data = $action instanceof ReadPrefill ?  $inputWidget->prepareDataSheetToPrefill() : $inputWidget->prepareDataSheetToRead();
+                                $this->addData($data, $action, $child);
+                            }
+                            yield from $this->generateModelForWidget($child, ($linkDepth-1));
+                            break;
+                        case $action instanceof iShowWidget:
+                            if (null !== $childActionWidget = $child->getAction()->getWidget()) {
+                                $this->addRoute(new PWARoute($this, $this->getViewForWidget($childActionWidget)->getRouteName(), $childActionWidget));
+                                yield 'Action widget for page ' . $widget->getPage()->getAliasWithNamespace() . ': ' . $widget->getId();
+                                yield from $this->generateModelForWidget($childActionWidget, ($linkDepth-1));
+                            } elseif (null !== $childActionPage = $child->getAction()->getPage()) {
+                                yield from $this->generateModelForWidget($childActionPage->getWidgetRoot(), ($linkDepth-1));
+                            }
+                            break;
+                    }
+                    break;
+                default: 
+                    yield from $this->generateModelForWidget($child, ($linkDepth-1));
             }
         }
     }
     
-    protected function getRouteOfflineStrategy(PWARouteInterface $route) : string
+    protected function isWidgetLazyLoading(WidgetInterface $widget) : bool
     {
-        $action = $route->getAction();
-        $triggerWidget = $route->getTriggerWidget();
-        $inputWidget = $route->getTriggerInputWidget();
-        if ($action !== null) {
-            $strategy = $action->getOfflineStrategy();
-            switch (true) {
-                case $strategy !== null: return $strategy;
-                case $action instanceof ShowHelpDialog: return OfflineStrategyDataType::ONLINE_ONLY;
-                case $inputWidget !== null && $inputWidget->isHidden(): return OfflineStrategyDataType::USE_CACHE;
-                // TODO what offline strategy do non-lazy dialogs get?
-                case $action instanceof iShowWidget: return OfflineStrategyDataType::PRESYNC;
-            }
+        switch (true) {
+            case $widget instanceof Dialog:
+            case $widget instanceof Data:
+                $default = true;
+                break;
+            default:
+                $default = false;
         }
-        return OfflineStrategyDataType::PRESYNC;
+        return $widget->getLazyLoading($default);
+    }
+    
+    protected function getActionOfflineStrategy(ActionInterface $action) : string
+    {
+        $triggerWidget = $this->getActionWidget($action);
+        $strategy = $action->getOfflineStrategy();
+        switch (true) {
+            case $strategy !== null: 
+                return $strategy;
+            case $action instanceof RefreshWidget:
+            case $action instanceof CallWidgetFunction:
+                return OfflineStrategyDataType::CLIENT_SIDE;
+            case $action instanceof iExportData:
+                return OfflineStrategyDataType::ENQUEUE;
+            case $this->isAvailableOffline() === true && $action instanceof iReadData: 
+                return OfflineStrategyDataType::PRESYNC;
+            case $this->isAvailableOfflineHelp() === false && $action instanceof ShowHelpDialog:
+                return OfflineStrategyDataType::ONLINE_ONLY;
+            case $this->isAvailableOfflineUnpublished() === false && $triggerWidget && $triggerWidget->getPage()->isPublished() === false: 
+                return OfflineStrategyDataType::ONLINE_ONLY;
+            // TODO what offline strategy do non-lazy dialogs get?
+            case $action instanceof iShowWidget: 
+                return OfflineStrategyDataType::PRESYNC;
+            case $action instanceof iNavigate:
+                return OfflineStrategyDataType::CLIENT_SIDE;
+        }
+        return OfflineStrategyDataType::ENQUEUE;
     }
     
     protected function getWebapp() : Webapp
@@ -85,9 +158,6 @@ class UI5PWA extends AbstractPWA
     
     protected function getViewForWidget(WidgetInterface $widget) : UI5ViewInterface
     {
-        // IMPORTANT: generate the view first to allow it to add controller methods!
-        $view = $this->getWebapp()->getViewForWidget($widget);
-        $controller = $view->getController();
-        return $view;
+        return $this->getWebapp()->getViewForWidget($widget);
     }
 }
