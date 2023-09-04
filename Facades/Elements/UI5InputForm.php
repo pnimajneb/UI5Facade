dd389d9b242e332e24f0e67c6901232f15e25980
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-09-04 22:32:23 +0200

    NEW widgets InputForm and InputFormDesigner

---
+++ Facades/Elements/UI5InputForm.php
@@ -0,0 +1,152 @@
+<?php
+namespace exface\UI5Facade\Facades\Elements;
+
+use exface\UI5Facade\Facades\Interfaces\UI5ControllerInterface;
+use exface\Core\DataTypes\StringDataType;
+use exface\Core\Facades\AbstractAjaxFacade\Elements\SurveyJsTrait;
+
+/**
+ * Creates a Survey-JS instance for an InputForm widget
+ * 
+ * @method \exface\Core\Widgets\InputForm getWidget()
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class UI5InputForm extends UI5Input
+{
+    use SurveyJsTrait {
+        buildJsSurveyInitOptions AS buildJsSurveyInitOptionsViaTrait;
+    }
+    
+    const CONTROLLER_VAR_SURVEY = 'survey';
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5Input::buildJsConstructorForMainControl()
+     */
+    public function buildJsConstructorForMainControl($oControllerJs = 'oController')
+    {
+        $controller = $this->getController();
+        $controller->addDependentObject(self::CONTROLLER_VAR_SURVEY, $this, 'null');
+        
+        $this->registerExternalModules($controller);
+        
+        return <<<JS
+
+        new sap.ui.core.HTML("{$this->getId()}", {
+            content: "<div id=\"{$this->getIdOfSurveyDiv()}\"></div>",
+            afterRendering: function() {
+                {$this->buildJsSurveySetup()}
+
+                var oValueBinding = new sap.ui.model.Binding(sap.ui.getCore().byId('{$this->getId()}').getModel(), '{$this->getValueBindingPath()}', sap.ui.getCore().byId('{$this->getId()}').getModel().getContext('{$this->getValueBindingPath()}'));
+                oValueBinding.attachChange(function(oEvent){
+                    {$this->buildJsValueSetter("sap.ui.getCore().byId('{$this->getId()}').getModel().getProperty('{$this->getValueBindingPath()}')")};
+                });
+            }
+        })
+
+JS;
+    }
+    
+    protected function buildJsSurveyVar() : string
+    {
+        return $this->getController()->buildJsDependentObjectGetter(self::CONTROLLER_VAR_SURVEY, $this);
+    }
+    
+    /**
+     * 
+     * @see SurveyJsTrait::buildJsSurveyConfigGetter()
+     */
+    protected function buildJsSurveyModelGetter() : string
+    {
+        $widget = $this->getWidget();
+        $model = $this->getView()->getModel();
+        if ($model->hasBinding($widget, 'form_config')) {
+            $modelPath = $model->getBindingPath($widget, 'form_config');
+        } else {
+            $modelPath = $this->getValueBindingPrefix() . $this->getWidget()->getFormConfigDataColumnName();
+        }
+        return "sap.ui.getCore().byId('{$this->getId()}').getModel().getProperty('{$modelPath}')";
+    }
+    
+    /**
+     * 
+     * @see SurveyJsTrait::buildJsSurveyInitOptions()
+     */
+    protected function buildJsSurveyInitOptions(string $oSurveyJs = 'oSurvey') : string
+    {
+        return $this->buildJsSurveyInitOptionsViaTrait($oSurveyJs) . <<<JS
+    
+    $oSurveyJs.onUpdateQuestionCssClasses.add(function(_, options) {
+        const classes = options.cssClasses;
+        if (classes.headerLeft === 'title-left') {
+            classes.titleLeftRoot += ' sapUiRespGrid sapUiRespGridHSpace0 sapUiRespGridVSpace0 sapUiFormResGridCont sapUiRespGridOverflowHidden sapUiRespGridMedia-Std-LargeDesktop';
+            /* TODO replace class sapUiRespGridMedia-Std-LargeDesktop to match current device in UI5 */
+            classes.headerLeft += ' sapUiRespGridSpanXL5 sapUiRespGridSpanL4 sapUiRespGridSpanM4 sapUiRespGridSpanS12';
+        }
+    });
+
+JS;
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Facades\AbstractAjaxFacade\Elements\JqueryInputValidationTrait::buildJsValidator()
+     */
+    public function buildJsValidator(?string $valJs = null)
+    {
+        // TODO
+        return "true";
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5AbstractElement::registerExternalModules()
+     */
+    public function registerExternalModules(UI5ControllerInterface $controller) : UI5AbstractElement
+    {
+        foreach ($this->getJsIncludes() as $src) {
+            $name = StringDataType::substringAfter($src, '/', $src, false, true);
+            $name = str_replace('-', '_', $name);
+            
+            $name = 'libs.exface.survey.' . $name;
+            $controller->addExternalModule($name, $src);
+        }
+        
+        foreach ($this->getCssIncludes() as $src) {
+            $controller->addExternalCss($src);
+        }
+        
+        return $this;
+    }
+    
+    /**
+     *
+     * @return string[]
+     */
+    protected function getJsIncludes() : array
+    {
+        $htmlTagsArray = $this->buildHtmlHeadTagsForSurvey();
+        $tags = implode('', $htmlTagsArray);
+        $jsTags = [];
+        preg_match_all('#<script[^>]*src="([^"]*)"[^>]*></script>#is', $tags, $jsTags);
+        return $jsTags[1];
+    }
+    
+    /**
+     *
+     * @return string[]
+     */
+    protected function getCssIncludes() : array
+    {
+        $htmlTagsArray = $this->buildHtmlHeadTagsForSurvey();
+        $tags = implode('', $htmlTagsArray);
+        $jsTags = [];
+        preg_match_all('#<link[^>]*href="([^"]*)"[^>]*/?>#is', $tags, $jsTags);
+        return $jsTags[1];
+    }
+}
---
+++ Facades/Elements/UI5InputFormDesigner.php
@@ -0,0 +1,136 @@
+<?php
+namespace exface\UI5Facade\Facades\Elements;
+
+use exface\UI5Facade\Facades\Interfaces\UI5ControllerInterface;
+use exface\Core\DataTypes\StringDataType;
+use exface\Core\Facades\AbstractAjaxFacade\Elements\SurveyJsTrait;
+use exface\Core\Exceptions\Facades\FacadeLogicError;
+use exface\Core\DataTypes\FilePathDataType;
+
+/**
+ * Creates a Survery-JS instance for an InputForm widget
+ * 
+ * @method \exface\Core\Widgets\InputForm getWidget()
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class UI5InputFormDesigner extends UI5InputForm
+{
+    const CONTROLLER_VAR_CREATOR = 'creator';
+    
+    protected function init()
+    {
+        parent::init();
+        
+        $creatorPath = FilePathDataType::normalize($this->getFacade()->getConfig()->getOption('LIBS.SURVEY.CREATOR_JS'), DIRECTORY_SEPARATOR);
+        if (! file_exists($this->getWorkbench()->filemanager()->getPathToVendorFolder() . DIRECTORY_SEPARATOR . $creatorPath)) {
+            throw new FacadeLogicError('Cannot initialize InputFormDesigner widget: Survey JS Creator is not installed!');
+        }
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5Input::buildJsConstructorForMainControl()
+     */
+    public function buildJsConstructorForMainControl($oControllerJs = 'oController')
+    {
+        $controller = $this->getController();
+        $controller->addDependentObject(self::CONTROLLER_VAR_CREATOR, $this, 'null');
+        
+        $this->registerExternalModules($controller);
+        
+        return <<<JS
+
+        new sap.ui.core.HTML("{$this->getId()}", {
+            content: "<div id=\"{$this->getIdOfCreatorDiv()}\"></div>",
+            afterRendering: function() {
+                var oCreator;
+                SurveyCreator.StylesManager.applyTheme("default");
+                SurveyCreator.localization.currentLocale = "{$this->getSurveyLocale()}";
+                    
+                // Show Designer, Test Survey, JSON Editor and additionally Logic tabs
+                var options = {
+                    showLogicTab: true
+                };
+                //create the SurveyJS Creator and render it in div with id equals to "creatorElement"
+                oCreator = new SurveyCreator.SurveyCreator("{$this->getIdOfCreatorDiv()}", options);
+                //Show toolbox in the right container. It is shown on the left by default
+                oCreator.showToolbox = "left";
+                //Show property grid in the right container, combined with toolbox
+                oCreator.showPropertyGrid = "right";
+
+                {$this->buildJsCreatorVar()} = oCreator;
+                
+                setTimeout(function(){
+                    $('.svd_commercial_container').attr('style', 'display:none!important');
+                }, 100);
+
+                var oValueBinding = new sap.ui.model.Binding(sap.ui.getCore().byId('{$this->getId()}').getModel(), '{$this->getValueBindingPath()}', sap.ui.getCore().byId('{$this->getId()}').getModel().getContext('{$this->getValueBindingPath()}'));
+                oValueBinding.attachChange(function(oEvent){
+                    {$this->buildJsValueSetter("sap.ui.getCore().byId('{$this->getId()}').getModel().getProperty('{$this->getValueBindingPath()}')")};
+                });
+            }
+        })
+
+JS;
+    }
+    
+    protected function buildJsCreatorVar() : string
+    {
+        return $this->getController()->buildJsDependentObjectGetter(self::CONTROLLER_VAR_CREATOR, $this);
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5InputForm::buildJsSurveyConfigGetter()
+     */
+    protected function buildJsSurveyConfigGetter() : string
+    {
+        return $this->buildJsValueGetter();
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5InputForm::buildJsValidator()
+     */
+    public function buildJsValidator(?string $valJs = null)
+    {
+        // TODO
+        return "true";
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5AbstractElement::buildJsValueSetter()
+     */
+    public function buildJsValueSetter($value)
+    {
+        return $this->buildJsCreatorValueSetter($value);
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5AbstractElement::buildJsValueGetter()
+     */
+    public function buildJsValueGetter()
+    {
+        return $this->buildJsCreatorValueGetter();
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5Input::buildJsSetDisabled()
+     */
+    public function buildJsSetDisabled(bool $trueOrFalse, bool $resetWidgetOnChange = false) : string
+    {
+        // TODO
+        return '';
+    }
+}
