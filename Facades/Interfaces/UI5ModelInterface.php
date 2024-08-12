efa763847daa1c13415fc13c52f72a166649ea59
Andrej Kabachnik <kabachello@users.noreply.github.com>
2024-08-12 17:45:26 +0200

    FIX improved UI5 model binding detection

--- Facades/Interfaces/UI5ModelInterface.php
+++ Facades/Interfaces/UI5ModelInterface.php
@@ -34,7 +34,11 @@ interface UI5ModelInterface extends PrefillModelInterface
     public function getBindingPath(WidgetInterface $widget, string $bindingName) : string;
     
     /**
-     * Returns TRUE if the model contains other bindings with the same name (but for other widgets).
+     * Returns TRUE if the model already contains other bindings with the same name and path.
+     * 
+     * This is important to be able to check, if a control MAY get a model binding if it
+     * does not have one explicitly. This method checks, if the given widget and binding
+     * name would produce a binding path, that already exists, but has a differen meaning.
      * 
      * @param WidgetInterface $widget
      * @param string $bindingName
--- UI5Model.php
+++ UI5Model.php
@@ -8,6 +8,7 @@ use exface\Core\Interfaces\Widgets\iShowSingleAttribute;
 use exface\Core\Interfaces\Widgets\iShowDataColumn;
 use exface\Core\Interfaces\Widgets\PrefillModelInterface;
 use exface\Core\Widgets\Parts\PrefillModel;
+use exface\Core\CommonLogic\DataSheets\DataColumn;
 
 class UI5Model extends PrefillModel implements UI5ModelInterface
 {    
@@ -55,17 +56,23 @@ class UI5Model extends PrefillModel implements UI5ModelInterface
      */
     public function getBindingPath(WidgetInterface $widget, string $bindingName) : string
     {
+        $boundColumn = $this->getBindingDataColumn($widget, $bindingName);
+        return $boundColumn === null ? '' : '/' . $boundColumn->getName();
+    }
+    
+    protected function getBindingDataColumn(WidgetInterface $widget, string $bindingName) : ?DataColumn
+    {
         $binding = $this->getBinding($widget, $bindingName);
         
         if ($binding === null) {
-            return '';
+            return null;
         }
         
         if ($binding instanceof DataPointerInterface) {
-            return '/' . $binding->getColumn()->getName();
+            return $binding->getColumn();
         }
         
-        return '';
+        return null;
     }
     
     /**
@@ -75,37 +82,74 @@ class UI5Model extends PrefillModel implements UI5ModelInterface
      */
     public function hasBindingConflict(WidgetInterface $widget, string $bindingName) : bool
     {
-        // If there is not binding for the given widget and property, check if they conflict
-        // with another binding
-        if (! $this->hasBinding($widget, $bindingName)) {
-            // Iterate through bindings looking for those with the same binding name, but a different
-            // widget id.
-            foreach ($this->getBoundWidgetIds() as $widgetId) {
-                $bindings = $this->getBindingsForWidgetId($widgetId);
-                if ($bindings[$bindingName] !== null && $widgetId !== $widget->getId()) {
-                    $bindingWidget = $widget->getPage()->getWidget($widgetId);
-                    // If the other binding's widget or the current widget are not showing a single attribute,
-                    // it's really strange - so treat that as a potential conflict :)
-                    if (! ($bindingWidget instanceof iShowSingleAttribute) || ! ($widget instanceof iShowSingleAttribute)) {
-                        return true;
-                    }
-                    // Same goes for the case, that one of the widgets is not bound to a data column
-                    if (! ($bindingWidget instanceof iShowDataColumn) || ! ($widget instanceof iShowDataColumn)) {
-                        return true;
-                    }
-                    
-                    // If both can be bound to an attribute, but at least one is not, AND both have
-                    // the same data column name - it's OK, the same value is meant.
-                    if ((! $bindingWidget->isBoundToAttribute() || ! $widget->isBoundToAttribute()) && $bindingWidget->getDataColumnName() === $widget->getDataColumnName()) {
-                        continue;
-                    }
-                    // If both are bound to an attribute, but the attributes are different, it's a conflict!
-                    if ($bindingWidget->isBoundToAttribute() && $widget->isBoundToAttribute() && ! $bindingWidget->getAttribute()->is($widget->getAttribute())) {
-                        return true;
-                    }
+        // If there is a binding for the given widget and property, don't bother
+        if ($this->hasBinding($widget, $bindingName)) {
+            return false;
+        }
+        
+        // If the given widget is not showing a single attribute,
+        // it's really strange (what to bind???) - so treat that as a potential conflict :)
+        if (! ($widget instanceof iShowSingleAttribute)) {
+            return true;
+        }
+        // Same goes for the case, that the widget is not showing a data column
+        if (! ($widget instanceof iShowDataColumn)) {
+            return true;
+        }
+        
+        // See if the widget has a data column at all. If not, it will not produce a conflict!
+        $widgetColumnName = $widget->getDataColumnName();
+        if ($widgetColumnName === null) {
+            return false;
+        }
+        
+        // Iterate through existing bindings looking for those with the same 
+        // binding name and path, but a different meaning
+        foreach ($this->getBoundWidgetIds() as $widgetId) {
+            // If it's the same widget id, there cannot be any conflicts.
+            if ($widgetId === $widget->getId()) {
+                continue;
+            }
+            
+            // If the binding is empty, there is no conflict either
+            $bindingPointerWithSameName = $this->getBindingsForWidgetId($widgetId)[$bindingName] ?? null;
+            if ($bindingPointerWithSameName === null) {
+                continue;
+            }
+            
+            $bindingWidget = $widget->getPage()->getWidget($widgetId);
+            
+            // If the other binding's widget is not showing a single attribute,
+            // it's really strange - so treat that as a potential conflict :)
+            if (! ($bindingWidget instanceof iShowSingleAttribute)) {
+                return true;
+            }
+            // Same goes for the case, that the other widget is not showing a single data column
+            if (! ($bindingWidget instanceof iShowDataColumn)) {
+                return true;
+            }
+            
+            // Now check if the resulting binding path is the same and if the
+            // meaning (e.g. attribute) might be different
+            $boundColumn = $bindingPointerWithSameName->getColumn();
+            $bindingColumnName = $boundColumn === null ? null : $boundColumn->getName();
+            
+            // Conflicts may arise if the data column name is the same for different widgets
+            // showing semantically different things
+            if ($bindingColumnName === $widgetColumnName) {
+                // If both can be bound to an attribute, but at least one is not, AND both have
+                // the same data column name - it's OK, the same value is meant.
+                if ((! $bindingWidget->isBoundToAttribute() || ! $widget->isBoundToAttribute())) {
+                    continue;
+                }
+                
+                // If both are bound to the same data column BUT the attributes are different, it's a conflict!
+                if (! $bindingWidget->getAttribute()->is($widget->getAttribute())) {
+                    return true;
                 }
             }
         }
+        
         return false;
     }
 }
