c2b1f0910b3a273ff88f8cefcab8069b08180792
Andrej Kabachnik <kabachello@users.noreply.github.com>
2024-09-23 16:46:48 +0200

    DEV NavTiles with IconTabBar

--- Facades/Elements/UI5NavTiles.php
+++ Facades/Elements/UI5NavTiles.php
@@ -1,5 +1,8 @@
 <?php
 namespace exface\UI5Facade\Facades\Elements;
+use exface\Core\DataTypes\StringDataType;
+use exface\Core\Widgets\Tile;
+use exface\Core\Widgets\Tiles;
 
 /**
  * Renders a default container for NavTiles.
@@ -54,8 +57,87 @@ class UI5NavTiles extends UI5Container
 
 JS;
         }
-        
-        return parent::buildJsChildrenConstructors();
+        if ($this->hasIconTabBar() === true) {
+            $navbar = $this->buildJsIconTabBar();
+        } else {
+            '';
+        }
+        return $navbar . ', ' . parent::buildJsChildrenConstructors();
+    }
+
+    protected function buildJsIconTabBar() : string
+    {
+        $this->getController()->addOnEventScript($this, 'TabSelect', <<<JS
+
+            var oPanel;
+            // get the selected key
+            var sKey = oEvent.getParameter("key");
+
+             // Find the corresponding panel that matches the key dynamically
+            var oView = this.getView();
+            oPanel = sap.ui.getCore().byId(sKey);
+            if (oPanel && oPanel.getDomRef()) {
+                oPanel.getDomRef().scrollIntoView({ behavior: "smooth" })
+            }
+JS);
+
+        $this->getController()->addOnEventScript($this, 'FilterTiles', <<<JS
+            console.log('searching');
+JS);
+
+        return <<<JS
+
+        new sap.m.FlexBox("{$this->getId()}_navbox", {
+            justifyContent: "SpaceBetween",
+            backgroundDesign: "Solid",
+            items: [
+                new sap.m.IconTabHeader("{$this->getId()}_iconTabHeader", {
+                    mode: "Inline",
+                    select: {$this->getController()->buildJsEventHandler($this, 'TabSelect', true)},
+                    items: [
+                        {$this->buildJsIconTabBarItems()}
+                    ]
+                }).addStyleClass('customHeader'),
+                new sap.m.SearchField({
+                    placeholder: "Search...",
+                    liveChange: {$this->getController()->buildJsEventHandler($this, 'FilterTiles', true)},
+                    width: "100%"
+                }).addStyleClass('customSearchField sapUiNoContentPadding')
+            ]
+        }).addStyleClass('responsiveFlexbox')
+JS;
+    }
+
+    protected function buildJsIconTabBarItems() : string
+    {
+        $js = '';
+        foreach ($this->getWidget()->getTiles() as $i => $tileGroup) {
+            if ($i === 0) {
+                $tileGroup->setHidden(true);
+                continue;
+            }
+            $js .= $this->buildJsIconTabBarItem($tileGroup);
+        }
+        return $js;
+    }
+
+    protected function buildJsIconTabBarItem(Tiles $tileGroup) : string
+    {
+        $tabCaption = StringDataType::substringAfter($tileGroup->getCaption(), ' > ');
+        $tabElement = $this->getFacade()->getElement($tileGroup);
+        return <<<JS
+
+                new sap.m.IconTabFilter({
+                    key: "{$tabElement->getId()}",
+                    text: "{$tabCaption}"
+                }),
+JS;
+    }
+
+    protected function hasIconTabBar() : bool
+    {
+        //return $this->getWidget()->getDepth() > 1;
+        return true;
     }
     
     /**
